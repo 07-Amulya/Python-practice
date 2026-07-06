f945187a176ad9043c1493026906ace0caf7b172
Ammu <amulya.j@diggibyte.com>
2026-07-06 16:01:06 +0530

    Day 4 and 5 practiced

---
+++ src/higher_order_func.py
@@ -0,0 +1,15 @@
+"""
+def myfunc(n):
+  return len(n)
+
+x = map(myfunc, ('apple', 'banana', 'cherry'))
+
+
+
+
+
+def myfunc(a, b):
+  return a + b
+
+x = map(myfunc, ('apple', 'banana', 'cherry'), ('orange', 'lemon', 'pineapple'))
+"""
